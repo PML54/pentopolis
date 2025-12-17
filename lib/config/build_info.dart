962ee0cc62ffa04991412457bb880c97b4162d5f
PML <paul.lariviere@wanadoo.fr>
2025-12-17 04:50:01 +0100

    classic OK 17 dec 04:49

--- lib/config/build_info.dart
+++ lib/config/build_info.dart
@@ -1,6 +1,6 @@
 // GÉNÉRÉ AUTOMATIQUEMENT par scripts/update_version.sh
 // NE PAS MODIFIER MANUELLEMENT
-// Dernière génération : 15/12/2025 à 17:55
+// Dernière génération : 17/12/2025 à 04:45
 
 /// Informations de build de l'application
 class BuildInfo {
@@ -8,10 +8,10 @@ class BuildInfo {
   static const String version = '1.0.1';
 
   /// Numéro de build (format YYYYMMDDHHMM)
-  static const int buildNumber = 202512151755;
+  static const int buildNumber = 202512170445;
 
   /// Date et heure du build (ISO 8601)
-  static const String buildDate = '2025-12-15T17:55:22';
+  static const String buildDate = '2025-12-17T04:45:26';
 
   /// Date formatée pour affichage
   static String get buildDateFormatted {
